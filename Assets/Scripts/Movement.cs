288ce6c890b80b53c5832666dd45f9eff7942644
BuildTools <unconfigured@null.spigotmc.org>
2020-11-04 21:06:59 +0000

    Added Render texture and dummy ui template... Fixed movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,78 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Movement : MonoBehaviour
 {
+    //Variables
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    private Vector3 move = Vector3.zero;
 
-    public Transform follow;
-    bool canMove = true;
-    bool canMoveForward = true;
-
-    bool forward = false;
-    bool turnRight = false;
-    bool turnLeft = false;
-
-    public void Move(string key)
+    void Update()
     {
-        if (key == "w" && canMove && canMoveForward)
+        CharacterController controller = GetComponent<CharacterController>();
+        // check if grounded
+        if (controller.isGrounded)
         {
-            canMove = false;
-            forward = true;
-            StartCoroutine(Interval(1));
-        } else if (key == "a" && canMove)
-        {
-            canMove = false;
-            turnLeft = true;
-            StartCoroutine(Interval(2));
-        } else if (key == "d" && canMove)
-        {
-            canMove = false;
-            turnRight = true;
-            StartCoroutine(Interval(2));
-        }
-    }
+            //get input
+            move = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            move = transform.TransformDirection(move);
+            //speeeed?
+            move *= speed;
 
-    private void FixedUpdate()
-    {
-        if (forward)
-        {
-            transform.position = Vector3.Lerp(transform.position, follow.position, Time.deltaTime);
         }
-
-        if (turnLeft)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90, 0), 3 * Time.deltaTime);
-        }
-
-        if (turnRight)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 90, 0), 3 * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Wall")
-        {
-            canMoveForward = false;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Wall")
-        {
-            canMoveForward = true;
-        }
-    }
-
-    IEnumerator Interval(int t)
-    {
-        yield return new WaitForSeconds(t);
-        forward = false;
-        turnRight = false;
-        turnLeft = false;
-        canMove = true;
+        //gravity
+        move.y -= gravity * Time.deltaTime;
+        //make character move
+        controller.Move(move * Time.deltaTime * 2);
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * 0.5f);
     }
 }
